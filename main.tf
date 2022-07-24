55b92dfaaa2c2f0c42866001b3c829aeef427bad
pwm <pwm@google.com>
2022-07-24 21:07:19 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "project" {
+    type = string
+}
+
+provider "google" {
+  project = var.project
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  tags         = ["ssh"]
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20220204"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
