456baf1fcde9ec724278e963b0b0af470aef4b67
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-12-06 20:58:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -e
+
+fail() {
+    echo "$@"
+    exit 1
+}
+
 echo "begin to deploy using gcloud..."
 echo $vm_name
 gcloud compute instances create $vm_name \
     --network-interface=network-tier=PREMIUM,subnet=$subnetwork \
     --zone=$zone \
-    --project=$project_id
+    --project=$project_id || fail "create vm failed"
