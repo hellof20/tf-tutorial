1a27f7f31a606cf519fbfed825301053173085ba
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-12-06 18:30:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "begin to deploy using gcloud..."
+echo $vm_name
 gcloud compute instances create $vm_name \
     --network-interface=network-tier=PREMIUM,subnet=$subnetwork \
     --zone=$zone \
